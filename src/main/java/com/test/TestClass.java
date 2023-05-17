d1a4fcceeb170b8af46980257ff6e7b37c0e4663
xdsswar <xdsswar@gmail.com>
2023-05-17 16:58:43 -0400

    Implement new Way to create Tray Icons in JavaFX

--- src/main/java/com/test/TestClass.java
+++ src/main/java/com/test/TestClass.java
@@ -1,9 +1,12 @@
 package com.test;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import xss.it.Assets;
 import xss.it.tray.NiceTrayIcon;
@@ -18,12 +21,26 @@ public class TestClass extends Application {
     @Override
     public void start(Stage stage){
         trayIcon=new NiceTrayIcon(new Image(Assets.load("/icon-16x16.png").toExternalForm()));
-
         ContextMenu menu=new ContextMenu();
-        MenuItem item=new MenuItem("Exit");
-        menu.getItems().add(item);
-
+        MenuItem item=new MenuItem("Open Window");
+        MenuItem item1=new MenuItem("Exit");
+        menu.getItems().addAll(item,item1);
         trayIcon.setContextMenu(menu);
+
+        stage.setScene(new Scene(new AnchorPane(),1000,500));
+
+        item.setOnAction(event -> {
+            stage.show();
+        });
+
+        trayIcon.setOnAction(event -> {
+            if (event.isDoubleClick()){
+               Platform.runLater(stage::show);
+            }
+        });
+
+        item1.setOnAction(event -> Platform.exit());
+
         trayIcon.install();
     }
 
